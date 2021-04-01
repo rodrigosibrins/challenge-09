ce744673097a74c47185c85d7d3190fb1b51220f
Rodri <rodrigosib2017@gmail.com>
2021-04-01 14:59:11 -0300

    function that controls the correct href register.html anchor

--- js/login.js
+++ js/login.js
@@ -26,5 +26,6 @@ function inputsRightAmount(){
     }
 }
 
+
 formExist();
 inputsRightAmount();
--- js/register.js
+++ js/register.js
@@ -1,6 +1,7 @@
 let testScreen = document.getElementById('test');
 let textValidations = document.getElementById('validations');
 let form = document.querySelector('form');
+
 function formExist(){
     if(document.forms = true){
         console.log("Validation Passed!");
@@ -26,6 +27,36 @@ function inputsRightAmount(){
     }
 }
 
+function inputsAreRequired(){
+    let inpName = form.elements[0];
+    let inpEmail = form.elements[1];
+    let inpPassword = form.elements[2];
+    let inpConfirmPass = form.elements[3];
+
+    if(inpName.hasAttribute('required') && inpEmail.hasAttribute('required') &&
+       inpPassword.hasAttribute('required') && inpConfirmPass.hasAttribute('required')){
+        console.log("Validation Passed!");
+        return true;
+       }else{
+        console.log("la batiste")
+       }
+}
+
+function anchorRight(){
+    let anchorHref = document.getElementById('anchor').href;
+    if(anchorHref == 'http://127.0.0.1:5500/login.html'){
+        console.log("Validation Passed");
+        return true;
+    }else{
+        const liWrongHref = `<li>Wrong Href</li>`;
+        testScreen.style.display = 'flex';
+        testScreen.style.border = '2px solid red';
+        textValidations.innerHTML += liWrongHref;
+    }
+}
+
 
 formExist();
 inputsRightAmount();
+inputsAreRequired();
+anchorRight();
