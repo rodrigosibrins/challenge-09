4271c684e3898fbe9356f1a45c6d86b8902be59c
Rodri <rodrigosib2017@gmail.com>
2021-04-01 13:41:44 -0300

    function that controls inputs amount

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 let testScreen = document.getElementById('test');
 let textValidations = document.getElementById('validations');
+let form = document.querySelector('form');
 function formExist(){
     if(document.forms = true){
         console.log("Validation Passed!");
@@ -12,4 +13,18 @@ function formExist(){
     }   
 }
 
+function inputsRightAmount(){
+    if(form.elements['email'] && form.elements['password']){
+        console.log("Validation Passed!")
+        return true;
+    }else{
+        const liMissingInp = `<li>Missing Input</li>`;
+        testScreen.style.display = 'flex';
+        testScreen.style.border = '2px solid red';
+        textValidations.innerHTML += liMissingInp;
+        console.log("error");
+    }
+}
+
 formExist();
+inputsRightAmount();
--- js/register.js
+++ js/register.js
@@ -1,15 +1,31 @@
 let testScreen = document.getElementById('test');
 let textValidations = document.getElementById('validations');
+let form = document.querySelector('form');
 function formExist(){
     if(document.forms = true){
         console.log("Validation Passed!");
         return true;
     }else{
-        const liForm = `<li>Form element doesn't exist</li>`
+        const liForm = `<li>Form element doesn't exist</li>`;
         testScreen.style.display = 'flex';
         testScreen.style.border = '2px solid red';
         textValidations.innerHTML += liForm;
     }   
 }
 
+function inputsRightAmount(){
+    if(form.elements['name'] && form.elements['email'] && form.elements['password'] &&
+       form.elements['confirm-password']){
+        console.log("Validation Passed!")
+        return true;
+    }else{
+        const liMissingInp = `<li>Missing Input</li>`;
+        testScreen.style.display = 'flex';
+        testScreen.style.border = '2px solid red';
+        textValidations.innerHTML += liMissingInp;
+    }
+}
+
+
 formExist();
+inputsRightAmount();
